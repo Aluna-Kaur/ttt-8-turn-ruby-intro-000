03c52363b744f3bf9cd681a724c947ee3169be77
Aluna Kaur <aluna.singh89@gmail.com>
2021-03-05 05:23:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     board = move(board, index)
-    return board
+    display_board(board)
   else
     turn(board)
   end
